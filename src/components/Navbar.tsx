7e058b98d2209b89daa332b8205e5a849cd1d9e7
Robin Spiltijns <robinspiltijns@MacBook-Pro-6.lan>
2023-05-07 12:22:52 +0200

    add homepage link to icon in nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { useState } from 'react';
+import Link from "next/link";
+
 
 export default function Navbar() {
     const [showMenu, setShowMenu] = useState(false);
@@ -12,16 +14,18 @@ export default function Navbar() {
 
     return (
         <nav className={`z-50 sticky top-0 w-full ${showMenu ? 'bg-black' : 'bg-light-purple'}`}>
-            <div className={`flex flex-row justify-between align-middle px-5 py-3 border-b-2 ${showMenu ? 'border-white' : 'border-black'}`}>
-                {!showMenu && <Image priority src="/logo.png" alt="comatch-logo" width={62} height={48}/>}
-                {showMenu && <Image priority src="/logo-white.png" alt="comatch-logo" width={62} height={48}/>}
-                <button onClick={toggleMenu}>
-                    <svg className="h-8 w-8" stroke={showMenu ? 'white' : 'black'}>
-                        {!showMenu && <path strokeWidth="3" d="M4 8h24M4 16h24M4 24h24"/>}
-                        {showMenu && <path strokeWidth="3" d="M4 8L28 32M4 32L28 8"/>}
-                    </svg>
-                </button>
-            </div>
+            <Link href="/">
+                <div className={`flex flex-row justify-between align-middle px-5 py-3 border-b-2 ${showMenu ? 'border-white' : 'border-black'}`}>
+                    {!showMenu && <Image priority src="/logo.png" alt="comatch-logo" width={62} height={48}/>}
+                    {showMenu && <Image priority src="/logo-white.png" alt="comatch-logo" width={62} height={48}/>}
+                    <button onClick={toggleMenu}>
+                        <svg className="h-8 w-8" stroke={showMenu ? 'white' : 'black'}>
+                            {!showMenu && <path strokeWidth="3" d="M4 8h24M4 16h24M4 24h24"/>}
+                            {showMenu && <path strokeWidth="3" d="M4 8L28 32M4 32L28 8"/>}
+                        </svg>
+                    </button>
+                </div>
+            </Link>
             {showMenu && (
                 <div className={`absolute w-full bg-black`}>
                     <div className="font-mono text-white px-5 py-3 border-b-2">
