aa2321a315b19b732d2bb98309aa40c5371ba5d3
Robin Spiltijns <robinspiltijns@MacBook-Pro-6.local>
2023-05-28 00:09:02 +0200

    place listing blocked by auth

--- src/app/post-listing/page.tsx
+++ src/app/post-listing/page.tsx
@@ -1,13 +1,26 @@
+'use client'
+
 import GoogleSignIn from "@/components/GoogleSignIn"
-import Image from "next/image"
+import { auth } from "@/lib/firebase"
+import { useState } from "react"
+
+const initialAuthState = auth.currentUser
 
 export default function PostListing() {
+    const [authState, setAuthState] = useState(initialAuthState)
+    auth.onAuthStateChanged(autState => {
+        setAuthState(autState)
+    })
+
     return (
-        <div className="p-5">
-            <h1 className="font-mono text-2xl mb-7">
-                Log in or sign up
-            </h1>
-            <GoogleSignIn/>
+        <div>
+            <h2 className="font-mono text-2xl py-2 px-5 border-b-2 border-dotted">
+                Place listing
+            </h2>
+            <div className="p-5">
+                {authState == null && (<GoogleSignIn/>)}
+                {authState != null && (<div>To add flow for posting cohouse.</div>)}
+            </div>
         </div>
     )
 }
