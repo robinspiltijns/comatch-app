f0d525586e5b588bde3f56dd1303d624cb9927d0
Robin Spiltijns <robinspiltijns@MacBook-Pro-6.local>
2023-05-27 19:45:21 +0200

    sort results by creationDate

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -1,18 +1,22 @@
 import ListingSummary from "@/components/ListingSummary"
 import { db } from "@/lib/firebase";
-import { ListingSummarySchema, ListingSummaryType } from "@/lib/schema";
-import { collection, getDocs } from "firebase/firestore"; 
+import { ListingSummaryType, docToListingSummary } from "@/lib/schema";
+import { collection, getDocs, orderBy, query } from "firebase/firestore"; 
 
 async function getListingSummaries(): Promise<ListingSummaryType[]> {
-    const parsedDocs = (await getDocs(collection(db, "listings"))).docs
-        .map(doc => ListingSummarySchema.safeParse(doc.data()))
-
+    const sortedListingCollection = await getDocs(query(collection(db, "listings"), orderBy("creationDate", "desc")))
+    const docs = (sortedListingCollection).docs.map(doc => doc.data())
     const listingSummaries: ListingSummaryType[] = []
-    for (let parsedDoc of parsedDocs) {
+    console.log(docs);
+    
+    for (let doc of docs) {
+        const parsedDoc = docToListingSummary.safeParse(doc)
         if (parsedDoc.success) {
             listingSummaries.push(parsedDoc.data)
         } else {
-            console.error(`Failed to parse document: ${JSON.stringify(parsedDoc.error)}`);
+            console.error(
+                `Failed to parse document\n${JSON.stringify(doc)}.\nError: ${JSON.stringify(parsedDoc.error)}}\n`
+            );
         }
     }
     return listingSummaries
@@ -20,41 +24,16 @@ async function getListingSummaries(): Promise<ListingSummaryType[]> {
 
 async function Listings() {
     // TODO: Decide on ISR vs dynamic data fetching
-    const listings = await getListingSummaries() 
-
+    const listingSummaries = await getListingSummaries() 
+    
     return (
         <div className="p-5 flex flex-col gap-5">
-            <ListingSummary
-                id="ULID1"
-                thumbnail="/house.jpg"
-                title="Cute boi group zoekt nieuwe cohouser"
-                moveInDate={new Date("2023-04-29")}
-                domicile={true}
-                housemates={5}
-                ageRange={[20,25]}
-                price={500}
+            {listingSummaries.map(listingSummary => (
+                <ListingSummary
+                key={listingSummary.id}
+                {...listingSummary}
             />
-            <ListingSummary
-                id="ULID2"
-                thumbnail="/house2.png"
-                title="Hype gang looking for gangster in Heverlee"
-                moveInDate={new Date("2023-09-01")}
-                domicile={false}
-                housemates={2}
-                ageRange={[30,35]}
-                price={450}
-            />
-            <ListingSummary
-                id="ULID3"
-                thumbnail="/house.jpg"
-                title="Rustige jongeren zoeken daddy"
-                moveInDate={new Date("2023-12-01")}
-                domicile={true}
-                housemates={3}
-                ageRange={[20,23]}
-                price={543}
-            />
-            <p>Listings amount on firebase: {listings.length}</p>
+            ))}
         </div>
     )
 }
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,14 +1,18 @@
+import { Timestamp } from "firebase/firestore";
 import { z } from "zod";
 
-export const ListingSummarySchema = z.object({
+export const docToListingSummary = z.object({
     id: z.string(),
     thumbnail: z.string(), // more strict type?
     title: z.string(),
-    moveInDate: z.date(),
+    moveInDate: z.instanceof(Timestamp),
     domicile: z.boolean(),
     housemates: z.number().int(),
     ageRange: z.tuple([z.number().int(), z.number().int()]),
     price: z.number()
-})
+}).transform(data => ({
+    ...data,
+    moveInDate: data.moveInDate.toDate()
+}))
 
-export type ListingSummaryType = z.infer<typeof ListingSummarySchema>
+export type ListingSummaryType = z.output<typeof docToListingSummary>
