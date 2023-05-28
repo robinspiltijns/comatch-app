e5e7cf57ca78bcc144eafc7720626215dd143332
Robin Spiltijns <robinspiltijns@MacBook-Pro-6.home>
2023-05-28 15:10:46 +0200

    try to open in native browser

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -6,16 +6,26 @@ import Image from "next/image"
 
 const handleLogin = () => signInWithPopup(auth, googleAuthProvider);
 
+const openInBrowser = () => {
+    window.open(window.location.href, '_system');
+  }
+
 function GoogleSignIn() {
     return (
-        <button onClick={handleLogin} className="bg-white border-2 rounded-xl w-full py-3 px-4 font-sans">
-            <div className="flex flex-row items-center relative">
-                <Image src="/google.png" alt="google icon" width={25} height={20}/>
-                <div className="font-sans absolute text-center w-full">
-                    Continue with google
+        <div>
+            <button onClick={handleLogin} className="bg-white border-2 rounded-xl w-full py-3 px-4 font-sans">
+                <div className="flex flex-row items-center relative">
+                    <Image src="/google.png" alt="google icon" width={25} height={20}/>
+                    <div className="font-sans absolute text-center w-full">
+                        Continue with google
+                    </div>
                 </div>
-            </div>
-        </button>
+                {window.navigator.userAgent}
+            </button>
+            <button onClick={openInBrowser}>
+                Open in browser
+            </button>
+        </div>
     )
 }
 
