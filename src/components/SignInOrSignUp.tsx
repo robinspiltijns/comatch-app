340c7a5bee83c88c7f82ebe0c4b1a33ebbaa1c1e
Robin Spiltijns <robinspiltijns@MacBook-Pro-6.home>
2023-05-28 16:26:34 +0200

    Add logic for showing loading first and check messenger webview

--- src/components/SignInOrSignUp.tsx
+++ src/components/SignInOrSignUp.tsx
@@ -3,24 +3,61 @@
 import { auth, googleAuthProvider } from "@/lib/firebase";
 import { signInWithPopup } from "firebase/auth";
 import Image from "next/image"
+import { useEffect, useState } from "react";
+
+type Loading = {
+    type: "LOADING"
+}
+
+type Loaded = {
+    type: "LOADED",
+}
+
+type LoadingState = Loading | Loaded
 
 const handleLogin = () => signInWithPopup(auth, googleAuthProvider);
 
 function SignInOrSignUp() {
-      
-    return (
-        <div>
-            <button onClick={handleLogin} className="bg-white border-2 rounded-xl w-full py-3 px-4 font-sans">
-                <div className="flex flex-row items-center relative">
-                    <Image src="/google.png" alt="google icon" width={25} height={20}/>
-                    <div className="font-sans absolute text-center w-full">
-                        Continue with google
+
+    useEffect(() => {
+        if (window.navigator.userAgent.includes('Messenger')) 
+            setSupportedUserAgent(false)
+    }, [])
+
+    const [loadingState, setLoadingState] = useState<LoadingState>({type: "LOADING"})
+    const [supportedUserAgent, setSupportedUserAgent] = useState(true)
+
+    // TODO: Make this based on auth context.
+    auth.onAuthStateChanged(_ => {
+        console.log("SetState");
+        setLoadingState({type: "LOADED",})
+    })  
+
+    switch(loadingState.type) {
+        case "LOADING": return (
+            <div>Loading</div>
+        )
+        case "LOADED": return (
+            <div>
+                {supportedUserAgent && (
+                    <button onClick={handleLogin} className="bg-white border-2 rounded-xl w-full py-3 px-4 font-sans">
+                    <div className="flex flex-row items-center relative">
+                        <Image src="/google.png" alt="google icon" width={25} height={20}/>
+                        <div className="font-sans absolute text-center w-full">
+                            Continue with google
+                        </div>
                     </div>
-                </div>
-                {window.navigator.userAgent}
-            </button>
-        </div>
-    )
+                    {window.navigator.userAgent}
+                </button>
+                )}
+                {!supportedUserAgent && (
+                    <div>
+                        Not supported
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
 export default SignInOrSignUp
